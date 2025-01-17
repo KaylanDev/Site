2184cd57979f4d5b56a965e5de410dedf04f7096
KaylanDev <k1ndealblack@gmail.com>
2025-01-16 22:56:04 -0300

    primeiro commit

---
+++ SiteMotos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SiteMotos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
