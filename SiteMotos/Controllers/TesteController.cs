43df4ac1914ea9c076c345d488f76f799a6f7800
KaylanDev <k1ndealblack@gmail.com>
2025-01-19 02:04:26 -0300

    tentando aplicar uma barra de pesquisa falta adaptar

---
+++ SiteMotos/Controllers/TesteController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SiteMotos.Controllers
+{
+    public class TesteController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<string> ola = new List<string>(){
+        "MT-07",
+        "MT-09",
+        "MT-15",
+        "MT-10",
+
+    };
+
+
+
+            return View(ola);
+        }
+    }
+}
